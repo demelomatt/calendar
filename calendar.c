9e32a3001ce279fdecfc04062c9eb487f6ab581d
Matheus Melo <demelosmatheus@outlook.com>
2020-05-30 22:55:00 -0300

    Update calendar.c

--- calendar.c
+++ calendar.c
@@ -1,7 +1,10 @@
+//importação de bibliotecas
 #include <stdio.h>
+//declaração de variáveis globais
 int n1,n2;
+//atribuição de funções globais
 int main(){
-    //declaração de variáveis
+    //declaração de variáveis locais
     int i,month,year,monthdays,day,weekday; 
     char months[12][10] = {"Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"
     };
@@ -11,38 +14,38 @@ int main(){
     printf("Digite o mês desejado: ");
     scanf("%d", & month);
     printf("Digite o ano desejado: ");
-    scanf("%d",& year);
-    //atribuição de funções
-    int printdays(int n1,int n2){
-        for(i = n1;i<monthdays;i++){
-            if(i<10){
+    scanf("%d", & year);
+    //atribuição de funções locais
+    void printdays(int n1,int n2){
+        for(i = n1; i < monthdays; i++){
+            if(i < 10){
                 printf("   %d",i+1);
             }
             else{
                 printf("  %d",i+1);
             }
-            if(((i+1)+n2)%7==0 && i<monthdays-1){
+            if(((i+1) + n2) % 7==0 && i < monthdays-1){
                 printf("\n");
                 i+=1;
                 printf(" %d",i+1);
             }
         }
-        return 0;
+        return ;
     }
     //imprimir dias da semana
-    printf("\n        %s\ %d\n",months[month-1],year);
-    for (i=0; i<7;i++){
+    printf("\n        %s %d\n",months[month-1],year);
+    for (i=0; i < 7;i++){
         printf(days[i]);
     }
     printf("\n");
     //verificar quantidade de dias do mês
-    monthdays = 30 + (month + month/8)%2;
+    monthdays = 30 + (month + month/8) % 2;
     //verificar se ano é bissexto
-    if (month ==2 && year%4 == 0 && (year%100 != 0 || year%400 == 0)) {
+    if (month == 2 && year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
         monthdays = 29;
     }
     else {
-        if (month ==2){
+        if (month == 2){
             monthdays = 28;
         }
     }
@@ -55,7 +58,7 @@ int main(){
     else {
         day += year -2;
     }
-    weekday = (23*month/9 + day + 4 + year/4 - year/100 + year / 400) % 7;
+    weekday = (23 * month /9 + day + 4 + year /4 - year /100 + year /400) % 7;
     //imprimir calendário
     switch(weekday){    
         case 0:
